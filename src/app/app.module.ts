91ef3357d8a6b092b658dacdf7f3fd8f1cd68456
angular-quoctuan <nghiepuit.quoctuaninfo@gmail.com>
2017-07-21 04:13:49 +0700

    lan dau commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ColorPickerComponent } from './components/color-picker/color-picker.component';
+import { SizeSettingComponent } from './components/size-setting/size-setting.component';
+import { ResetComponent } from './components/reset/reset.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorPickerComponent,
+    SizeSettingComponent,
+    ResetComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
